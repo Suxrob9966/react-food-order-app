94f2b95923678bbb42118ccfc49c529757f51545
Suxrob9966 <rabbimovich9966@gmail.com>
2022-02-04 15:57:52 -0600

    added p tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h2>Let's get started!</h2>;
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>Finally figured it out</p>
+    </div>
+  );
 }
 
 export default App;
