817a5e060486fa61ba758daad49506db197730cf
Suxrob9966 <rabbimovich9966@gmail.com>
2022-02-13 21:22:53 -0600

    added individual meal item

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './MealItem.module.css';
+
+const MealItem = (props) => {
+  const price = `$${Number(props.price).toFixed(2)}`; // it was `$${props.price.toFixed(2)}`
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
